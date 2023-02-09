80ea63ba78cce5fa0ee7e5044f5627c796886510
Bhagyashri <bhagyashrideoghare116@gmail.com>
2023-02-09 18:13:32 +0530

    6-fetching the data and exploring the app

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,42 @@
-import { useState } from "react";
-import { Restaurantlist } from "./config";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 function filterData (searchText,restaurants)  {
   const filterData = restaurants.filter((restaurant) => 
-    restaurant.data.name.includes(searchText)
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
     )
     return filterData;
 }
 
 
 const Body = () => {
-
+        
+    const [filteredRestaurant, setFilteredRestaurant] = useState([])
+    const [allRestaurants, setAllRestaurant] = useState([]);
     const [searchText,setSearchText] = useState("");
-    const [restaurants, setRestaurant] = useState(Restaurantlist)
 
-    return(
+    useEffect(() => {
+       getRestaurant()
+    },[])
+
+    async function getRestaurant() {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.7913957&lng=85.0002336&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json()
+        console.log(json)
+        setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards)
+        setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards)
+    }
+
+    if(!allRestaurants) return null;
+
+    if(filteredRestaurant.length === 0){
+        return <h2>No Restaurant Found</h2>
+    }
+             
+    return (allRestaurants?.length === 0) ? (<Shimmer/>
+    ) : (
+    
         <>
         <div className="search-container">
             <input type="text"
@@ -28,14 +49,14 @@ const Body = () => {
             />
             <button className="search-btn"
              onClick={() => {
-               const data = filterData(searchText,restaurants) 
-               setRestaurant(data)
+               const data = filterData(searchText,allRestaurants) 
+               setFilteredRestaurant(data)
              }}
              >search
             </button>
         </div>
         <div className="restaurant-list">
-            {restaurants.map((restaurant) => {
+            {filteredRestaurant.map((restaurant) => {
                 return <RestaurantCard {...restaurant.data} key= {restaurant.data.id}/>
             })
 
---
+++ src/components/Shimmer.js
@@ -0,0 +1,9 @@
+
+
+const Shimmer = () => {
+    return (
+
+        <h2>Shimmer UI loading.....</h2>
+    )
+}
+export default Shimmer
